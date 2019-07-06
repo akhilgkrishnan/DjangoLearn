2022eec4a48348b8ee9ae9a36e3c6bc9ba6004f0
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-06 13:29:08 +0530

    Add comments

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),#create a path to index page (index is function which is mentioned in the views)
 ]
