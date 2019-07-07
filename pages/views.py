d174f7082a2cd8ab86ff5137091b29744fa43d9c
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-07 22:16:11 +0530

    pass dynamic contents to templates

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import destination
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html",) #return the contents of index.html
+    dest = destination()
+    dest.id = 5
+    dest.name = 'Akhil'
+    dest.price = 500
+    return render(request, "index.html",{'dest':dest}) #return the contents of index.html
