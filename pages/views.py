d7c68ff5844f9d3cade8dd332d88341bcd441d80
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-08 13:43:15 +0530

    create an object to fetch all datas from the database

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,6 @@ from .models import destination
 
 # Create your views here.
 def index(request):
-    dest = destination()
-    dest.id = 5
-    dest.name = 'Akhil'
-    dest.price = 500
+    dest = destination.objects.all()
+    
     return render(request, "index.html",{'dest':dest}) #return dynamic contents to index.html
