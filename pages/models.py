a9160e34fc474e8e36153d51d2350fa509a30d9b
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-07-08 13:42:36 +0530

    create database schema for destination

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class destination:
-    id : int
-    name : str
-    desc : str
-    pri : int
+class destination(models.Model):
+    
+    name = models.CharField(max_length =100 ,default='SOME STRING')
+    image = models.ImageField(upload_to='pics', default='SOME STRING')
+    description = models.TextField(max_length=300, default='SOME STRING')
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
 
